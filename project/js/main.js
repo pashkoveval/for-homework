6365167a261fb9b76f28a18d27ae40d07e5c2e0c
Pashkov <pashkoveval@gmail.com>
2020-07-19 02:09:05 +0300

    первое дз
    сделал вроде все но не понял как подставить картинки под каждую категорию товара.почему то не передаеться значение с объекта в строку с кодом html

---
+++ project/js/main.js
@@ -0,0 +1,27 @@
+
+// неразобрался как всталять картинки под каждый слот свою картинку ;((((
+
+const products = [
+    { id: 1, title: 'Notebook', price: 120000, img: 'img/msi.jpg' },
+    { id: 2, title: 'Mouse', price: 1500, img: 'img/mause.jpg' },
+    { id: 3, title: 'Keyboard', price: 5000, img: 'img/keyboard.jpg' },
+    { id: 4, title: 'Gamepad', price: 4500, img: 'img/gamepad.jpg' },
+];
+
+const renderProduct = (title, price, img="http://placehold.it/300x200.png") => {
+    return `<div class="product-item">
+                <h3>${title}</h3>
+                <img src="${img}" alt="" class="img">
+                <p>${price}</p>
+                <button class="by-btn">Добавить в корзину</button>
+              </div>`;
+};
+
+const renderProducts = list => {
+    const productList = list.map(item => renderProduct(item.title, item.price));
+    console.log(productList);
+    // убераем запятую через .join(' ')
+    document.querySelector('.products').innerHTML = productList.join(' ');
+};
+
+renderProducts(products);
